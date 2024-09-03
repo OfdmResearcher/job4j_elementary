695525d56a3efd6a29d2eb41217759b8ab96fe21
Nikita <river600@rambler.ru>
2024-09-03 11:22:09 +0300

    5. Конвертер валюты с правильным EURO и dollar [#41583 #511666]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -3,19 +3,17 @@ package ru.job4j.converter;
 public class Converter {
 
     public static float rubleToEuro(float value) {
-        float result = value / 70;
-        return result;
+        return value / 100;
     }
 
     public static float rubleToDollar(float value) {
-        float result = value / 60;
-        return result;
+        return value / 90;
     }
 
     public static void main(String[] args) {
-        float euro = Converter.rubleToEuro(140);
+        float euro = Converter.rubleToEuro(200);
         System.out.println("140 rubles are " + euro + " euro.");
-        float dollar = Converter.rubleToDollar(120);
+        float dollar = Converter.rubleToDollar(180);
         System.out.println("120 rubles are " + dollar + " dollars.");
     }
 }
